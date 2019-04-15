2dacf368e44902d34b0091430084c11e88479dff
wei feng <theweifeng@gmail.com>
2019-04-15 17:37:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def dictionary
+  hash = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  hash = dictionary
+  
+  arr = string.split(" ")
+  
+  arr.each_with_index {|word, idx| arr[idx] = hash[word.downcase] if hash.keys.include?(word.downcase)}
+  
+  arr.join(" ")
+end
+
+def bulk_tweet_shortener(arr)
+  arr.each {|sentence| puts word_substituter(sentence)}
+end
